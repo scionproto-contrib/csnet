9b07e2dce4d32758ab88ba83474cfc5d759c9270
Colin Pfingstl <19822061+koflin@users.noreply.github.com>
2025-04-24 13:44:29 +0200

    chore: improve network setup with convenience script

--- scripts/run-testclient.sh
+++ scripts/run-testclient.sh
@@ -1,15 +1,8 @@
-if [ -z $SCION_TESTNET_PATH ]
-then
-  echo "SCION_TESTNET_PATH not set"
-  exit 1
-fi
+WORKSPACE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-if [ -z SCION_PATH ]
-then
-  echo "SCION_PATH not set"
-  exit 1
-fi
+export SCION_PATH="$WORKSPACE_DIR/network/scion"
+export SCION_TESTNET_PATH="$WORKSPACE_DIR/network/scion-testnet"
 
-cd $SCION_TESTNET_PATH
+cd "$SCION_TESTNET_PATH" || exit
 
-go run test-client.go -daemon '[fd00:f00d:cafe::7f00:54]:30255' -local '2-ff00:0:222,[fd00:f00d:cafe::7f00:55]:31000' -remote 1-ff00:0:133,127.0.0.101:31001 -data "abc"
+go run test-client.go -daemon '[fd00:f00d:cafe::7f00:54]:30255' -local '2-ff00:0:222,[fd00:f00d:cafe::7f00:55]:31002' -remote 1-ff00:0:133,127.0.0.101:31001 -data "abc"
